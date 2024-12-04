fa137b539d50c1ff1e8f0dbdbe6ff2cfff1967bd
seoeun Park <sseonxx@gmail.com>
2024-12-04 21:04:27 +0900

    first

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Home() {
+  return (
+    <View className="bg-red-100">
+      <Text>Home2</Text>
+    </View>
+  );
+}
